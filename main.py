74fb1f49514071416bd3e9578c5e53982918629c
monees007 <monees007@outlook.com>
2022-09-26 13:20:36 +0530

    Status changing tab implemented

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-from flask import Flask
-from database import db
+from flask import Flask, render_template
+
+# from database import db
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/model.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+dbs = sqlite3.connect('base.db')
+print("DB connected")
+
+dbs.execute(''' create table card(
+        title Varchar,
+        # id int primary key not null,
+        content varchar,
+        status boolean,
+        deadline date,
+        parent_list int,
+        date_created date);''')
+
